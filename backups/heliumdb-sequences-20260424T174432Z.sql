77ff7ace0ae740d2bc1af6f7a083b9b6c75b8714
ricardocidale <52429710-ricardocidale@users.noreply.replit.com>
2026-04-25 05:28:04 +0000

    Git commit prior to merge

--- backups/heliumdb-sequences-20260424T174432Z.sql
+++
@@ -1,4 +0,0 @@
-SELECT setval('__drizzle_migrations_id_seq', 21, true);
-SELECT setval('external_integrations_id_seq', 22, true);
-SELECT setval('login_logs_id_seq', 1287, true);
-SELECT setval('users_id_seq', 96, true);
